a1991b881021080bccc66facd0fbe7fe10b0e71f
Administrator <Administrator>
2025-12-12 03:40:11 -0500

    Signed-off-by: Administrator <Administrator>

--- InstallMIDIMapper.bat
+++
@@ -1,5 +0,0 @@
-DEL C:\Windows\System32\midimap.dll /F /Q
-DEL C:\Windows\SysWOW64\midimap.dll /F /Q
-RENAME C:\Windows\System32\MIDIMapper.dll midimap.dll
-RENAME C:\Windows\SysWOW64\MIDIMapper.dll midimap.dll
-pause
